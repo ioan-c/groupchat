362ad164767d9014dd52dba7df1a2dc39f6a8227
Ioan Conache <ionutz.conache@hotmail.com>
2019-04-26 15:44:20 +0300

    test commit 2

--- src/main/java/com/example/groupchat/controller/HomeController.java
+++ src/main/java/com/example/groupchat/controller/HomeController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class HomeController {
-//test comit
+//test comit 2ascasda
 //    @RequestMapping("/")
 //    public String index() {
 //        return "Greetings from Spring Boot!";
